4f8eed6a42cba86c1be1ade62d6b38fe042795d2
unoo <bki92@naver.com>
2022-08-07 15:26:14 +0900

    실습파일 2번째

--- Project1_1/Project1_1/main.cpp
+++ Project1_1/Project1_1/main.cpp
@@ -3,8 +3,14 @@ using namespace std;
 
 int main(void)
 {
-	cout << "hello world" << endl;
-	cout << "hi" << endl;
-	cout << "한글됩니까입니까" << endl;
-		return 0;
+	int currentHP = 100;
+	currentHP = 50;
+	cout << "현재 내 체력 : " << currentHP << endl;
+
+	int a = 10;
+	int b = 20;
+
+	cout << "a+b=" << a + b << endl;
+
+	return 0;
 }
