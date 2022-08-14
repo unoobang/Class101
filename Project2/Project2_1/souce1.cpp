81ff29035d88019847065115e0b4b9003218d579
unoo <bki92@naver.com>
2022-08-14 15:56:16 +0900

    2주차 실습파일

---
+++ Project2/Project2_1/souce1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int number;
+
+	cin >> number;
+
+	for (int i = 1; i <= number;i++)
+	{
+		cout << i << endl;
+	}
+
+	return 0;
+}
+
